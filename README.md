e7a2b6dc2a66ad096e0fa5cb949ae5205d8e3e41
Graham Boyle <olayile1999@gmail.com>
2024-01-07 21:44:07 +0100

    updated

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hover Board Project
+
+## just implementing my JS skills
+
+## click (here)[comingsoon...] to view project
---
+++ main.js
@@ -0,0 +1,43 @@
+const container = document.getElementById('container');
+const colors = [
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'indigo',
+  'violet',
+  'white',
+];
+const SQUARES = 500;
+const reset = document.getElementById('reset');
+
+for (let i = 0; i <= SQUARES; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+
+  square.addEventListener('mouseover', () => setColor(square));
+
+  // square.addEventListener('mouseout', () => removeColor(square)); --> Auto remove colors
+
+  reset.addEventListener('click', () => {
+    removeColor(square);
+  });
+
+  container.appendChild(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.background = color;
+  element.style.boxShadow = `0 0 3px ${color}`;
+}
+
+function removeColor(element) {
+  element.style.background = '#1d1d1d';
+  element.style.boxShadow = '0 0 2px #000';
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
